5ffc94a1a86a071d199c77ece6776a5cb1c199f7
Brandon-Magofna <magofna68@gmail.com>
2021-04-23 08:39:58 -0700

    creat Currency function

---
+++ src/Currency.js
@@ -0,0 +1,14 @@
+export default class Currency {
+  static getConversion(currency) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}?`)
+      .then(function (result) {
+        if (!Response.ok) {
+          throw Error(result.error - type);
+        }
+        return result.json();
+      })
+      .catch(function (error) {
+        return error;
+      })
+  }
+}
