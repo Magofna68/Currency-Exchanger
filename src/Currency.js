56d59c056bd9c8a3b7138136487ea2a48b12989e
Brandon-Magofna <magofna68@gmail.com>
2021-04-23 10:21:17 -0700

    WIP: posting data to html

--- src/Currency.js
+++ src/Currency.js
@@ -1,14 +1,32 @@
+// export default class Currency {
+//   static getConversion(currency, amount) {
+//     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}/${amount}`)
+//       .then(function (result) {
+//         if (!Response.ok) {
+//           throw Error(result.error - type);
+//         }
+//         return conversion_result.json();
+//       })
+//       .catch(function (error) {
+//         return error;
+//       })
+//   }
+// }
+
 export default class Currency {
-  static getConversion(currency) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}?`)
-      .then(function (result) {
-        if (!Response.ok) {
-          throw Error(result.error-type);
+  static getConversion() {
+    return new Promise(function (resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}/${amount}`;
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.result);
+        } else {
+          reject(request.result);
         }
-        return result.json();
-      })
-      .catch(function (error) {
-        return error;
-      })
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
   }
 }
--- src/main.js
+++ src/main.js
@@ -4,9 +4,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import Currency from "./Currency.js"
 
-Currency.getConversion(currency);
-Promise.prototype.then(function (result) {
-  if (result instanceof Error) {
-    throw Error('Currency-Exchange API error: ${result.error-type}');
-  }
+// Currency.getConversion(currency, amount);
+// Promise.prototype.then(function (result) {
+//   if (result instanceof Error) {
+//     throw Error('Currency-Exchange API error: ${result.error-type}');
+//   }
+//   const placeholder = conversion_result.json()
+// })
+
+$(document).ready(function () {
+  let promise = Currency.getConversion();
+  promise.then(function (result) {
+    const body = JSON.parse(conversion_result);
+    $(".currency").text(body);
+  })
 })
