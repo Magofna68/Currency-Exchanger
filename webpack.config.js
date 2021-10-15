f336cc594c1154365d8c97aa26547bf2698413bf
Brandon-Magofna <magofna68@gmail.com>
2021-10-15 16:38:50 -0700

    package.json update

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "currency-exchange",
+      title: "Currency-Exchange",
       template: "./src/index.html",
       inject: "body",
     }),
